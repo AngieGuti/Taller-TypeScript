6db48e435582afbee3f9015cdfa9ce3e773e88a9
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2024-10-20 21:26:30 -0500

    tarea 4

---
+++ ProyectoGuiado/scripts/main.js
@@ -0,0 +1,9 @@
+import { Aprendiz, NivelEducativo } from './aprendiz.js';
+export var ap = new Aprendiz("Angie Camila", "Gutiérrez Trujillo", "avatar.jpg", 19, NivelEducativo.UNIVERSIDAD);
+var aprendizTable = document.getElementById('aprendiz');
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height=100 </td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel Educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tbodyAprendiz);
+}
